74b1ed7f6bbf51dd4f650a7fffe1db915478871d
jcanovasa <jaimecanovas.a@gmail.com>
2022-09-26 09:47:29 +0200

    Initial commit

---
+++ src/main/java/Jaime/HolaMundo/App.java
@@ -0,0 +1,7 @@
+package Jaime.HolaMundo;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hola Antonio! Este es mi programita en Java con Docker.");
+  }
+}
